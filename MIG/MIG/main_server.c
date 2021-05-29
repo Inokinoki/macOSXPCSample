430966e29166396fcdcfa829041a48acbc909565
Weixuan XIAO <veyx.shaw@gmail.com>
2021-05-29 21:59:26 +0200

    Add basic MiG declaration

---
+++ MIG/MIG/main_server.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  MIG
+//
+//  Created by Inoki on 29/05/2021.
+//  Copyright © 2021 inoki. All rights reserved.
+//
+
+#include <stdio.h>
+#include <mach/mach.h>
+#include <mach/message.h>
+#include <servers/netname.h>
+#include <mach/mach_error.h>
+#include <stdlib.h>
+
+#include "add.h"
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
